1fd0f1e4a57755646b07d2b4370ffa6dc9aa4a5b
Gabriel Torelo <57268681+GabrielTorelo@users.noreply.github.com>
2024-07-05 15:01:34 -0300

    aula 479: introducao as variaveis

---
+++ 1 - Modulo 20/Dart - Fundamentos/aula2.dart
@@ -0,0 +1,10 @@
+void main() {
+  int idade = 24;
+  double altura = 1.80;
+  String nome = "Gabriel";
+  bool maiorDeIdade = true;
+  dynamic variavel = 1;
+
+  print(
+      "Nome: $nome, Idade: $idade, Altura: $altura, Maior de Idade: $maiorDeIdade, Variável: $variavel");
+}
