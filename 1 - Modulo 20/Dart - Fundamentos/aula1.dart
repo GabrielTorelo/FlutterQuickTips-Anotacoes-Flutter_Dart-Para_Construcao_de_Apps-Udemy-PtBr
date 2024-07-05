b605e7122d03398e465dd828368c78ce778fa0f5
Gabriel Torelo <57268681+GabrielTorelo@users.noreply.github.com>
2024-07-05 13:16:23 -0300

    aula 473-478: introducao a funcao 'main'

---
+++ 1 - Modulo 20/Dart - Fundamentos/aula1.dart
@@ -0,0 +1,12 @@
+void main() {
+  print("Olá Mundo!");
+}
+
+// O print abaixo não será executado (não está dentro da função "main")
+// print("Não serei executado");
+// Ao descomentar a linha acima, o código dará um erro de compilação
+
+// O bloco de código abaixo também não será executado
+void teste() {
+  print("Não serei executado");
+}
