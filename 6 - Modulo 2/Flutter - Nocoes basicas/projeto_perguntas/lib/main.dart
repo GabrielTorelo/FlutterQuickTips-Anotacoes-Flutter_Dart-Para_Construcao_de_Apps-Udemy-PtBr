6ca8345a6b4689564c50df794ec8e5f8e1f6f256
Gabriel Torelo <57268681+GabrielTorelo@users.noreply.github.com>
2024-07-17 08:52:50 -0300

    aula 35-40: alterando para widget stateful

--- 6 - Modulo 2/Flutter - Nocoes basicas/projeto_perguntas/lib/main.dart
+++ 6 - Modulo 2/Flutter - Nocoes basicas/projeto_perguntas/lib/main.dart
@@ -2,20 +2,29 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(PerguntaApp());
 
-// Para que a classe PerguntaApp seja reconhecida como um widget
-// É necessário que ela herde de "StatelessWidget" ou "StatefulWidget"
-//
-// StatelessWidget: Widget que não possui estado
-// StatefulWidget: Widget que possui estado
-//
-class PerguntaApp extends StatelessWidget {
-  // O decorator @override é opcional
-  // Ele indentifica que o método abaixo está sobrescrevendo um método da classe pai (StatelessWidget)
+class PerguntaAppState extends State<PerguntaApp> {
+  var perguntaSelecionada = 0;
+
+  // Método para responder a pergunta
+  void responder({required int opcao}) {
+    // setState: Método que atualiza o estado do widget
+    // O método build é chamado novamente
+    setState(() {
+      perguntaSelecionada++;
+      print('Resposta $opcao selecionada!');
+    });
+  }
+
   @override
   // O método build recebe um contexto e retorna um widget
   // Contexto: Informações sobre a árvore de widgets
   // Widget: Componente visual
   Widget build(BuildContext context) {
+    final List<String> perguntas = [
+      'Qual é a sua cor favorita?',
+      'Qual é o seu animal favorito?',
+    ];
+
     // MaterialApp: Widget que configura o tema da aplicação
     return MaterialApp(
         // home: Widget que será exibido na tela
@@ -32,30 +41,46 @@ class PerguntaApp extends StatelessWidget {
         // children: Lista de widgets que serão exibidos na tela
         children: [
           // Text: Widget que exibe um texto
-          Text(perguntas[0]),
+          Text(perguntas[perguntaSelecionada]),
           // ElevatedButton: Botão com elevação
           ElevatedButton(
             // onPressed: Evento que será executado ao clicar no botão
-            onPressed: () => print('Resposta 1 selecionada!'),
+            onPressed: () => responder(opcao: 1),
             // child: Texto do botão
             child: const Text('Resposta 1'),
           ),
           // ElevatedButton: Botão com elevação
           ElevatedButton(
             // onPressed: Evento que será executado ao clicar no botão
-            onPressed: () => print('Resposta 2 selecionada!'),
+            onPressed: () => responder(opcao: 2),
             // child: Texto do botão
             child: const Text('Resposta 2'),
           ),
           // ElevatedButton: Botão com elevação
           ElevatedButton(
             // onPressed: Evento que será executado ao clicar no botão
-            onPressed: () => print('Resposta 3 selecionada!'),
+            onPressed: () => responder(opcao: 3),
             // child: Texto do botão
             child: const Text('Resposta 3'),
           ),
         ],
       ),
     ));
+  }
+}
+
+// Para que a classe PerguntaApp seja reconhecida como um widget
+// É necessário que ela herde de "StatelessWidget" ou "StatefulWidget"
+//
+// StatelessWidget: Widget que não possui estado
+// StatefulWidget: Widget que possui estado
+//
+class PerguntaApp extends StatefulWidget {
+  // O decorator @override é opcional
+  // Ele indentifica que o método abaixo está sobrescrevendo um método da classe pai (StatefulWidget)
+  @override
+  // O método createState é responsável por criar o estado do widget
+  PerguntaAppState createState() {
+    return PerguntaAppState();
   }
 }
