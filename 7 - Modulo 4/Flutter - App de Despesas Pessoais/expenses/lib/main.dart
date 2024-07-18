cb316e48b22cd3ab3a5b783e5e05159d053fc897
Gabriel Torelo <gabrieltorelo@chiptronic.com.br>
2024-07-18 10:16:35 -0300

    aula 80-84: iniciando o projeto de despesas pessoais

---
+++ 7 - Modulo 4/Flutter - App de Despesas Pessoais/expenses/lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const ExpensesApp());
+
+class ExpensesApp extends StatelessWidget {
+  const ExpensesApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Expenses',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Personal Expenses'),
+        ),
+        body: const Center(
+          child: Text('Initial Version'),
+        ),
+      ),
+    );
+  }
+}
