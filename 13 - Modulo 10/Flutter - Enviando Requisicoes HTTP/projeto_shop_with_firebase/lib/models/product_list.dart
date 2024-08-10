4646664e9461b8744fca838e8c4493030a2182ad
Gabriel Torelo <gabrieltorelo@chiptronic.com.br>
2024-08-09 22:42:48 -0300

    feat: adds project from previous module

---
+++ 13 - Modulo 10/Flutter - Enviando Requisicoes HTTP/projeto_shop_with_firebase/lib/models/product_list.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:shop/MOCK/MOCK_DATA.dart';
+import 'package:shop/models/product.dart';
+import 'package:uuid/uuid.dart';
+
+class ProductList with ChangeNotifier {
+  final List<Product> _products = MOCK_PRODUCTS_DATA;
+
+  List<Product> get products => [..._products];
+
+  List<Product> get favoriteProducts {
+    return _products.where((product) => product.isFavorite).toList();
+  }
+
+  int get itemsCount {
+    return _products.length;
+  }
+
+  void saveProduct(Map<String, dynamic> data) {
+    bool hasId = data.containsKey('id');
+
+    final product = Product(
+      id: hasId ? data['id'] : const Uuid().v4(),
+      title: data['title'],
+      description: data['description'],
+      price: double.parse(data['price']),
+      imageUrl: data['imageUrl'],
+    );
+
+    if (hasId) {
+      updateProduct(product);
+    } else {
+      addProduct(product);
+    }
+  }
+
+  void addProduct(Product product) {
+    _products.add(product);
+    notifyListeners();
+  }
+
+  void updateProduct(Product product) {
+    final productIndex = _products.indexWhere((prod) => prod.id == product.id);
+
+    if (productIndex >= 0) {
+      _products[productIndex] = product;
+      notifyListeners();
+    }
+  }
+
+  void removeProduct(Product product) {
+    bool hasProductId =
+        _products.indexWhere((prod) => prod.id == product.id) >= 0;
+
+    if (hasProductId) {
+      _products.remove(product);
+      notifyListeners();
+    }
+  }
+}
