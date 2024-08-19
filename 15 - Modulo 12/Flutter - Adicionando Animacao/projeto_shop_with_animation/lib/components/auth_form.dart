fc4b3bdbb956f648324a33a2ac07db1af5ccf59f
Gabriel Torelo <57268681+GabrielTorelo@users.noreply.github.com>
2024-08-19 13:16:13 -0300

    aula 317-320: introducao aos widgets animados

--- 15 - Modulo 12/Flutter - Adicionando Animacao/projeto_shop_with_animation/lib/components/auth_form.dart
+++ 15 - Modulo 12/Flutter - Adicionando Animacao/projeto_shop_with_animation/lib/components/auth_form.dart
@@ -11,7 +11,8 @@ class AuthForm extends StatefulWidget {
   State<AuthForm> createState() => _AuthFormState();
 }
 
-class _AuthFormState extends State<AuthForm> {
+class _AuthFormState extends State<AuthForm>
+    with SingleTickerProviderStateMixin {
   final _passwordController = TextEditingController();
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
   AuthMode _authMode = AuthMode.login;
@@ -20,13 +21,47 @@ class _AuthFormState extends State<AuthForm> {
     'password': '',
   };
   bool _isLoading = false;
+  AnimationController? _controller;
+  Animation<double>? _opacityAnimation;
 
   bool _isLogin() => _authMode == AuthMode.login;
   bool _isSignup() => _authMode == AuthMode.signup;
 
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 300),
+    );
+
+    _opacityAnimation = Tween<double>(
+      begin: 0.0,
+      end: 1.0,
+    ).animate(
+      CurvedAnimation(
+        parent: _controller!,
+        curve: Curves.linear,
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _controller?.dispose();
+  }
+
   void _switchAuthMode() {
     setState(() {
-      _isLogin() ? _authMode = AuthMode.signup : _authMode = AuthMode.login;
+      if (_isLogin()) {
+        _authMode = AuthMode.signup;
+        _controller?.forward();
+      } else {
+        _authMode = AuthMode.login;
+        _controller?.reverse();
+      }
     });
   }
 
@@ -34,7 +69,10 @@ class _AuthFormState extends State<AuthForm> {
     showDialog(
       context: context,
       builder: (ctx) => AlertError(
-        message: message,
+        message: switch (message) {
+          null || 'null' => null,
+          _ => message,
+        },
       ),
     );
   }
@@ -88,92 +126,108 @@ class _AuthFormState extends State<AuthForm> {
       ),
       child: Padding(
         padding: const EdgeInsets.all(16),
-        child: IntrinsicHeight(
-          child: IntrinsicWidth(
-            stepWidth: deviceSize.width * 0.65,
-            child: Form(
-              key: _formKey,
-              child: Column(
-                children: [
-                  TextFormField(
-                    decoration: const InputDecoration(
-                      labelText: 'E-mail',
-                    ),
-                    keyboardType: TextInputType.emailAddress,
-                    onSaved: (value) => _authData['email'] = value ?? '',
-                    validator: (value) {
-                      return switch (value?.trim()) {
-                        '' || null => 'Please enter a e-mail',
-                        _ => null,
-                      };
-                    },
-                  ),
-                  TextFormField(
-                    decoration: const InputDecoration(
-                      labelText: 'Password',
-                    ),
-                    keyboardType: TextInputType.visiblePassword,
-                    obscureText: true,
-                    controller: _passwordController,
-                    onSaved: (value) => _authData['password'] = value ?? '',
-                    validator: (value) {
-                      return switch (value?.trim()) {
-                        '' || null => 'Please enter a password',
-                        _ => _isSignup() && value!.trim().length < 8
-                            ? 'Password must be at least 8 characters long'
-                            : null,
-                      };
-                    },
-                  ),
-                  if (_isSignup())
+        child: AnimatedSize(
+          duration: const Duration(milliseconds: 300),
+          curve: Curves.linear,
+          child: IntrinsicHeight(
+            child: SizedBox(
+              width: deviceSize.width * 0.65,
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  children: [
                     TextFormField(
                       decoration: const InputDecoration(
-                        labelText: 'Repeat Password',
+                        labelText: 'E-mail',
+                      ),
+                      keyboardType: TextInputType.emailAddress,
+                      onSaved: (value) => _authData['email'] = value ?? '',
+                      validator: (value) {
+                        return switch (value?.trim()) {
+                          '' || null => 'Please enter a e-mail',
+                          _ => null,
+                        };
+                      },
+                    ),
+                    TextFormField(
+                      decoration: const InputDecoration(
+                        labelText: 'Password',
                       ),
                       keyboardType: TextInputType.visiblePassword,
                       obscureText: true,
+                      controller: _passwordController,
+                      onSaved: (value) => _authData['password'] = value ?? '',
                       validator: (value) {
                         return switch (value?.trim()) {
                           '' || null => 'Please enter a password',
-                          _ => value!.trim() != _passwordController.text
-                              ? 'Passwords must be identical'
+                          _ => _isSignup() && value!.trim().length < 8
+                              ? 'Password must be at least 8 characters long'
                               : null,
                         };
                       },
                     ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(
-                      vertical: 20,
-                    ),
-                    child: _isLoading
-                        ? const CircularProgressIndicator()
-                        : ElevatedButton(
-                            onPressed: _submit,
-                            style: ElevatedButton.styleFrom(
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(30),
-                              ),
-                              padding: const EdgeInsets.symmetric(
-                                horizontal: 30,
-                                vertical: 8,
-                              ),
-                              backgroundColor:
-                                  Theme.of(context).colorScheme.primary,
-                              foregroundColor: Colors.white,
-                            ),
-                            child: Text(
-                              _isLogin() ? 'ENTER' : 'REGISTER',
-                            ),
+                    AnimatedContainer(
+                      constraints: BoxConstraints(
+                        minHeight: _isSignup() ? 60 : 0,
+                        maxHeight: _isSignup() ? 120 : 0,
+                      ),
+                      duration: const Duration(milliseconds: 300),
+                      curve: Curves.linear,
+                      child: FadeTransition(
+                        opacity: _opacityAnimation!,
+                        child: TextFormField(
+                          decoration: const InputDecoration(
+                            labelText: 'Repeat Password',
                           ),
-                  ),
-                  const Spacer(),
-                  TextButton(
-                    onPressed: _switchAuthMode,
-                    child: Text(
-                      _isLogin() ? "Don't have an account yet?" : "Login now",
+                          keyboardType: TextInputType.visiblePassword,
+                          obscureText: true,
+                          validator: (value) {
+                            if (_isLogin()) return null;
+
+                            return switch (value?.trim()) {
+                              '' || null => 'Please enter a password',
+                              _ => value!.trim() != _passwordController.text
+                                  ? 'Passwords must be identical'
+                                  : null,
+                            };
+                          },
+                        ),
+                      ),
                     ),
-                  ),
-                ],
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                        vertical: 20,
+                      ),
+                      child: _isLoading
+                          ? const CircularProgressIndicator()
+                          : ElevatedButton(
+                              onPressed: _submit,
+                              style: ElevatedButton.styleFrom(
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(30),
+                                ),
+                                padding: const EdgeInsets.symmetric(
+                                  horizontal: 30,
+                                  vertical: 8,
+                                ),
+                                backgroundColor:
+                                    Theme.of(context).colorScheme.primary,
+                                foregroundColor: Colors.white,
+                              ),
+                              child: Text(
+                                _isLogin() ? 'ENTER' : 'REGISTER',
+                              ),
+                            ),
+                    ),
+                    const Spacer(),
+                    TextButton(
+                      onPressed: _switchAuthMode,
+                      child: Text(
+                        _isLogin() ? "Don't have an account yet?" : "Login now",
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
           ),
