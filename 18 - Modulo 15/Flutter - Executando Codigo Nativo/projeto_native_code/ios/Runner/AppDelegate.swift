c87df5a17b88827ae57aa78bde1c46cbbbf9e6f6
Gabriel Torelo <57268681+GabrielTorelo@users.noreply.github.com>
2024-09-21 23:30:13 -0300

    aula 422: criando funcao nativa (iOS)

--- 18 - Modulo 15/Flutter - Executando Codigo Nativo/projeto_native_code/ios/Runner/AppDelegate.swift
+++ 18 - Modulo 15/Flutter - Executando Codigo Nativo/projeto_native_code/ios/Runner/AppDelegate.swift
@@ -7,6 +7,29 @@ import UIKit
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    let controller : FlutterViewController = window?.rootViewController as! FlutterViewController
+    let nativoChannel = FlutterMethodChannel(
+      name: "com.example.projeto_native_code/sum",
+      binaryMessenger: controller.binaryMessenger
+    )
+    
+    nativoChannel.setMethodCallHandler({
+        (call: FlutterMethodCall, result: @escaping FlutterResult) -> Void in
+        
+        guard call.method == "calculateSum" else {
+            result(FlutterMethodNotImplemented)
+            return
+        }
+        
+        if let args = call.arguments as? [String: Any],
+           let a = args["a"] as? Int,
+           let b = args["b"] as? Int {
+            result(a + b)
+        } else {
+            result(-1)
+        }
+    })
+    
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
