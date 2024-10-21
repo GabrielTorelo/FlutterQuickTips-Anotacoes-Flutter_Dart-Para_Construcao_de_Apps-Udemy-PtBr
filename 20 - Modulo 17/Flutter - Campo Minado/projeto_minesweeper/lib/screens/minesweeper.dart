e7ab06b66a27ab48dca18a15cc2d810db6d2b078
Gabriel Torelo <57268681+GabrielTorelo@users.noreply.github.com>
2024-10-20 23:27:59 -0300

    aula 442: criando componente para exibir o resultado

--- 20 - Modulo 17/Flutter - Campo Minado/projeto_minesweeper/lib/screens/minesweeper.dart
+++ 20 - Modulo 17/Flutter - Campo Minado/projeto_minesweeper/lib/screens/minesweeper.dart
@@ -1,16 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:projeto_minesweeper/components/result.dart';
+import 'package:projeto_minesweeper/models/area.dart';
 
 class Minesweeper extends StatelessWidget {
   const Minesweeper({super.key});
 
+  void _restartGame() {
+    print('Restarting game...');
+  }
+
+  void onOpen(Area area) {
+    print('Opening area...');
+  }
+
+  void onToggleMark(Area area) {
+    print('Toggling mark...');
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Minesweeper'),
+      appBar: ResultWidget(
+        win: null,
+        onRestart: _restartGame,
       ),
       body: const Center(
-        child: Text('Minesweeper'),
+        child: Text(
+          'Minesweeper',
+        ),
       ),
     );
   }
